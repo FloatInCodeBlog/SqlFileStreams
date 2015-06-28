d8c61e00f16afda630dfe180b518c3fd3f35f7a7
Floatas <floatas@outlook.com>
2015-06-28 23:50:04 +0300

    Initial

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using SqlFileStreams.Models;
+
+namespace SqlFileStreams.Controllers
+{
+    public class FilesController : Controller
+    {
+        private SqlFileStreamsContext db = new SqlFileStreamsContext();
+
+        public ActionResult Index()
+        {
+            return View(db.FileModels.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FileModel fileModel = db.FileModels.Find(id);
+            if (fileModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fileModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Title,File")] FileViewModel fileModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var fileData = new MemoryStream();
+                fileModel.File.InputStream.CopyTo(fileData);
+
+                var file = new FileModel { Title = fileModel.Title, File = fileData.ToArray() };
+                db.FileModels.Add(file);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(fileModel);
+        }
+    }
+}
---
+++ Models/FileModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace SqlFileStreams.Models
+{
+    [Table("Files")]
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public byte[] File { get; set; }
+    }
+
+    public class FileViewModel
+    {
+        public string Title { get; set; }
+        public HttpPostedFileBase File { get; set; }
+    }
+}
